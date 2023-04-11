bca34c143a5fc83296a1d75eb24ef12583d45d6e
Ribhav Sharma <ribhavsharma2003@gmail.com>
2023-04-10 18:21:09 -0700

    added KMP and LCSS

--- project.java
+++ project.java
@@ -8,7 +8,9 @@ import java.util.List;
 
 public class project {
 
-   public static int search(String n, String m) { // Rabin Karp String Matching Algorithm
+    // Rabin Karp String Matching Algorithm
+
+   public static int RKSearch(String n, String m) { 
        int matches = 0;
        int nLen = n.length();
        int mLen = m.length();
@@ -31,7 +33,63 @@ public class project {
        return matches;
    }
 
-   public static String[] readOriginal(String filename){ // Reads content from original file into an array of sentences
+   // Knuth-Morris-Pratt Algorithm
+
+   public static int KMPSearch(String text, String pattern) {
+    int[] prefix = computePrefix(pattern);
+    int matches = 0;
+    int j = 0;
+    for (int i = 0; i < text.length(); i++) {
+        while (j > 0 && pattern.charAt(j) != text.charAt(i)) {
+            j = prefix[j - 1];
+        }
+        if (pattern.charAt(j) == text.charAt(i)) {
+            j++;
+        }
+        if (j == pattern.length()) {
+            matches++;
+            j = prefix[j - 1];
+        }
+    }
+    return matches;
+}
+
+   private static int[] computePrefix(String pattern) {
+    int[] prefix = new int[pattern.length()];
+    int j = 0;
+    for (int i = 1; i < pattern.length(); i++) {
+        while (j > 0 && pattern.charAt(j) != pattern.charAt(i)) {
+            j = prefix[j - 1];
+        }
+        if (pattern.charAt(j) == pattern.charAt(i)) {
+            j++;
+        }
+        prefix[i] = j;
+    }
+    return prefix;
+}
+
+// Longest Common Substring Algorithm
+  
+public static int LCSSSearch(String s1, String s2) {
+    int[][] table = new int[s1.length() + 1][s2.length() + 1];
+    for (int i = 0; i <= s1.length(); i++) {
+        for (int j = 0; j <= s2.length(); j++) {
+            if (i == 0 || j == 0) {
+                table[i][j] = 0;
+            } else if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                table[i][j] = table[i-1][j-1] + 1;
+            } else {
+                table[i][j] = Math.max(table[i-1][j], table[i][j-1]);
+            }
+        }
+    }
+    
+    // Return the length of the LCSS
+    return table[s1.length()][s2.length()];
+}
+
+public static String[] readOriginal(String filename){ // Reads content from original file into an array of sentences
       String contents = "";
       try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
          String line;
@@ -77,7 +135,7 @@ public class project {
          for (int i = 0; i < contents.length; i++) {
             for (int j = 0; j < othercontents.size(); j++) {
                if(contents[i].length()<=othercontents.get(j).length())
-               matches+=search(othercontents.get(j),contents[i]); // increment number of matches with every match made
+               matches+=RKSearch(othercontents.get(j),contents[i]); // increment number of matches with every match made
             }
          }
       
